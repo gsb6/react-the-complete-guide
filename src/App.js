f52446dedc7c9179415a0fd1738940876f71c45d
Gabriel Brunichaki <gabriel.salles@acertsis.com.br>
2020-03-16 12:21:24 -0300

    Section 5: Styling React Components & Elements

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
 import './App.css';
 import Person from './Person/Person';
+
+const StyledButton = styled.button`
+  padding: 8px;
+  background-color: ${props => props.alt ? 'red' : 'green'};
+  border: 1px solid blue;
+  color: white;
+  font: inherit;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};;
+    color: black;
+  }
+`;
 
 class App extends Component {
   state = {
@@ -16,6 +32,7 @@ class App extends Component {
   nameChangedHandler = (event, personId) => {
     const personIndex = this.state.persons.findIndex(p => p.id === personId);
     const person = { ...this.state.persons[personIndex] };
+
     person.name = event.target.value;
 
     const persons = [...this.state.persons];
@@ -39,14 +56,6 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-    }
-
     let persons = null;
 
     if (this.state.showPersons) {
@@ -65,11 +74,21 @@ class App extends Component {
       );
     }
 
+    const classes = [];
+
+    if (this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+
+    if (this.state.persons.length <= 1) {
+      classes.push('bold');
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
-        <button style={style} onClick={this.togglePersonHandler}>Toggle Persons</button>
+        <p className={classes.join(' ')}>This is really working!</p>
+        <StyledButton alt={this.state.showPersons} onClick={this.togglePersonHandler}>Toggle Persons</StyledButton>
         {persons}
       </div>
     );
